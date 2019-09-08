d01f8d14d2d0d9ccbac213e0b3d9df16c687dc3d
Homnibus <eseunes@gmail.com>
2019-09-08 18:25:12 +0200

    ms backend update index.html

--- ms_back_end/ms_back_end/wsgi.py
+++ ms_back_end/ms_back_end/wsgi.py
@@ -11,6 +11,49 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ms_back_end.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ms_back_end.settings")
 
+"""
+05/08/2018 -- correction partie 1
+Mise en place d'un correctif pour palier à un bug de passenger. Passenger étant l'outil utilisé sur A2hosting pour
+permettre à Apache d'utiliser un service Python.
+Le but est de revaloriser la varible PATH_INFO qui n'est pas correctement valorisé lors d'appel de type POST
+"""
+SCRIPT_NAME = ""
+
+
+class PassengerPathInfoFix(object):
+  """
+  Sets PATH_INFO from REQUEST_URI since Passenger doesn't provide it.
+  """
+
+  def __init__(self, app):
+    self.app = app
+
+  def __call__(self, environ, start_response):
+    from urllib.parse import unquote
+
+    environ["SCRIPT_NAME"] = SCRIPT_NAME
+
+    if "REQUEST_URI" in environ:
+      request_uri = unquote(environ["REQUEST_URI"])
+    else:
+      request_uri = environ["PATH_INFO"]
+    script_name = unquote(environ.get("SCRIPT_NAME", ""))
+    offset = (
+      request_uri.startswith(script_name) and len(environ["SCRIPT_NAME"]) or 0
+    )
+    environ["PATH_INFO"] = request_uri[offset:].split("?", 1)[0]
+
+    return self.app(environ, start_response)
+
+
+"""
+fin correction partie 1
+"""
 application = get_wsgi_application()
+
+# Correction partie 2
+application = PassengerPathInfoFix(application)
+# Fin correction partie 2
+
